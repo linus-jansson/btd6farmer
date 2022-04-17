028ac030a5a8df08c230ad61a01738ff536fd26d
Linus Jansson <linus.jansson2000@gmail.com>
2022-04-17 20:41:00 +0200

    Test abilites

--- test_scripts/abilites.py
+++ test_scripts/abilites.py
@@ -3,6 +3,10 @@ import keyboard, time
 prev = time.time()
 
 while True:
-    if time.time()-prev >= 3:
-        keyboard.send("3")
+    if time.time()-prev >= 1:
+        keyboard.send("1")
+        time.sleep(0.2)
+        keyboard.send("2")
+        time.sleep(0.2)
+        print(time.time()-prev)
         prev = time.time()
