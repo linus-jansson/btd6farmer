d243cbf4e69ea71f5dbad8b79f0b070debe01c65
Linus J <linus.jansson2000@gmail.com>
2022-07-16 21:26:24 +0200

    Make a copy of the gameplan for next game

--- Game.py
+++ Game.py
@@ -1,10 +1,12 @@
 import json
+import copy
 
 class Game:
     def __init__(self, instruction_path=r".\Instructions\Dark_Castle_Hard_Standard", game_plan_filename="instructions.json", game_settings_filename="setup.json"):
         self.settings = self.__load_settings(instruction_path + "\\" + game_settings_filename)
         self.game_plan = self.__load_plan(instruction_path + "\\" + game_plan_filename)
         self.first_round = self.__get_first_round()
+        self.__copy_game_plan = copy.deepcopy(self.game_plan)
 
     def __load_settings(self, path):
         """
@@ -33,12 +35,24 @@ class Game:
             tmp.append(int(key))
         tmp.sort()
         return tmp[0]
-
-
+    
+    def reset_game_plan(self):
+        self.game_plan = copy.deepcopy(self.__copy_game_plan)
 
 if __name__ == "__main__":
     from pprint import pprint
 
     instance = Game()
-    pprint(instance.game_plan)
-    print(instance.first_round)
+    print(id(instance.game_plan))
+    
+    old_game_plan = instance.game_plan
+
+    for key in instance.game_plan.keys():
+        for item in instance.game_plan[key]:
+            item["Done"] = True
+    
+    instance.reset_game_plan()
+
+    print(id(instance.game_plan))
+    print(instance.game_plan == old_game_plan)
+    print(instance.game_plan)
