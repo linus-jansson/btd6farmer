fbfeae51685024b080b4e461a6d7df6f4d501c7e
Linus J <linus.jansson2000@gmail.com>
2022-07-20 20:55:37 +0200

    Removed unused code
    also, calculate stats before opening stats file

--- log.py
+++ log.py
@@ -1,6 +1,5 @@
 import json
 import os
-from l_utils import __handle_time
 
 class Log:
     def __init__(self):
@@ -45,54 +44,39 @@ class Log:
         except IOError:
             print("file does not exist")
 
+
+        if did_win:
+            data["wins"] += 1
+        else:
+            data["loses"] += 1
+        
+        total_matches = (data["wins"] + data["loses"])
+        # winrate = total wins / total matches
+        winrate = data["wins"] / total_matches
+
+        # Convert to procent
+        procentage = round(winrate * 100, 4)
+        
+        # Push procentage to winrate
+        data["winrate"] = f"{procentage}%"
+
+        data["average_matchtime_seconds"] = (data["total_time"]  + match_time) / total_matches
+        
+        # new_total_time = old_total_time + current_match_time in seconds
+        data["total_time"] += match_time
+        
+        # average = total_time / total_matches_played
+        average_converted, unit = self.__handle_time(data["average_matchtime_seconds"])
+        
+        # Push average to dictionary
+        data["average_matchtime"] = f"{round(average_converted, 3)} {unit}"
+
+
         # Open as write
         with open("stats.json", "w") as outfile:        
-            if did_win:
-                data["wins"] += 1
-            else:
-                data["loses"] += 1
-            
-            total_matches = (data["wins"] + data["loses"])
-            # winrate = total wins / total matches
-            winrate = data["wins"] / total_matches
-
-            # Convert to procent
-            procentage = (round(winrate * 100, 4))
-            
-            # Push procentage to winrate
-            data["winrate"] = f"{procentage}%"
-
-            data["average_matchtime_seconds"] = (data["total_time"]  + match_time) / total_matches
-            
-            # new_total_time = old_total_time + current_match_time in seconds
-            data["total_time"] += match_time
-            
-            # average = total_time / total_matches_played
-            average_converted, unit = self.__handle_time(data["average_matchtime_seconds"])
-            
-            # Push average to dictionary
-            data["average_matchtime"] = f"{round(average_converted, 3)} {unit}"
-
-            outfile.write(json.dumps(data, indent=4))
+            outfile.write(json.dumps(data, indent=4)) # write stats to file
         
         return data
 
     def log(self, *kargs):
         print(*kargs)
-
-
-    def printStats(self, stats):
-        os.system("cls")
-        print("="*6)
-        if round(time.time() - start_time, 2) >= 60.0:
-            stats["Uptime"] = "{} minutes".format(round( (time.time() - start_time) / 60, 2)  )
-        elif round(time.time() - start_time, 2) / 60 >= 60.0:
-            stats["Uptime"] = "{} hours".format(round( (time.time() - start_time) / 60 / 60, 2) )
-        else:
-            stats["Uptime"] = "{} seconds".format(round(time.time() - start_time, 2))
-        
-        for key, value in stats.items():
-            print(f"{key.replace('_', ' ')}\t{value}")
-        print("="*6)
-
-
