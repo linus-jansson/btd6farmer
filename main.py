ef74a7c5705becc9a05433226f8ac9447f4705e7
Linus J <linus.jansson2000@gmail.com>
2022-07-23 20:32:57 +0200

    Cleanup and commenting

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
     def no_gameplan_exception():
         raise Exception("No valid argument for directory.. 'python main.py --gameplan_path <directory to gameplan>'")
 
+    # Retrives the gameplan from the command line and makes a Path object out of it
     gameplan_path = (Path(__file__).resolve().parent/sys.argv[sys.argv.index("--gameplan_path") + 1]) if "--gameplan_path" in sys.argv else no_gameplan_exception()
-    
-    print(gameplan_path)
 
     # Verify directory exist.
     if not gameplan_path.exists():
@@ -20,14 +19,10 @@ if __name__ == "__main__":
     if not gameplan_path.is_dir():
         print("Not a directory")
         no_gameplan_exception()
-
-
     
-    # TODO: Move all these prints to verbose only mode
-
     bot = Bot(instruction_path=gameplan_path, debug_mode=("--debug" in sys.argv), verbose_mode=("--verbose" in sys.argv))
-
     print("Setting up Bot...")
+    print("Using gameplan located in: " + str(gameplan_path))
     
     bot.initilize() # Initialize the bot (presses alt, etc)
 
